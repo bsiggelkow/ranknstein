e2ed5bd96f3f237a25543e149dc3ed9939eb0c4d
Bill Siggelkow <bsiggelkow@mac.com>
2011-08-23 08:42:32 -0400

    Refactored to use sinatra/base

--- app/ranknstein.rb
+++ app/ranknstein.rb
@@ -1,29 +1,34 @@
-require 'rubygems'
-require 'sinatra'
-require 'haml'
-require 'app/ranker'
+require 'sinatra/base'
+require_relative 'ranker'
 
-# Usage: partial :foo => renders _foo.haml
-helpers do
-  def partial(page, options={})
-    haml page, options.merge!(:layout => false)
+class Ranknstein < Sinatra::Base
+
+  set :public, File.dirname(__FILE__) + '/public'
+  set :views, File.dirname(__FILE__) + '/views'
+  
+  # Usage: partial :foo => renders _foo.haml
+  helpers do
+    def partial(page, options={})
+      haml page, options.merge!(:layout => false)
+    end
   end
-end
 
-get '/' do
-  haml :index
-end
+  get '/' do
+    haml :index
+  end
 
-get '/rank' do
-  query = params['query']
-  url = params['url']
-  redirect '/' unless (query && !query.empty?) && (url && !url.empty?)
+  get '/rank' do
+    query = params['query']
+    url = params['url']
+    redirect '/' unless (query && !query.empty?) && (url && !url.empty?)
 
-  ranker = Ranker.new(query, url)
-  rank, result = ranker.rank
-  haml :rank, :locals => {:rank => rank, :result => result, :query => query}
-end
+    ranker = Ranker.new(query, url)
+    rank, result = ranker.rank
+    haml :rank, :locals => {:rank => rank, :result => result, :query => query}
+  end
 
-get '/faq' do
-  haml :faq
+  get '/faq' do
+    haml :faq
+  end
+
 end
--- config.ru
+++ config.ru
@@ -1,12 +1,4 @@
-unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
-  $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
-end
-require 'bundler/setup'
-require 'app/ranknstein'
-
-Sinatra::Application.set(
-  :run => false,
-  :environment => :production
-)
- 
-run Sinatra::Application
+require 'bundler'
+Bundler.require
+require './app/ranknstein'
+run Ranknstein
